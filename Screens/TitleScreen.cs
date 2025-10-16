8cbaa47dda4d5d1e74332317c37f269b77a3357e
Harivansh Luchmun <hluchmun108@gmail.com>
2025-10-15 23:40:35 -0500

    Added mouse hover and keyboard input to menu list selection

--- Screens/TitleScreen.cs
+++ Screens/TitleScreen.cs
@@ -16,6 +16,7 @@ namespace newgame.Screens;
             "Start Game",
             "Quit"
         };
+        private int labelIndex = 0;
 
         public override void LoadContent()
         {
@@ -26,22 +27,63 @@ namespace newgame.Screens;
 
         public override void Update(GameTime gameTime)
         {
+            // Track mouse position
+            var mouseState = Microsoft.Xna.Framework.Input.Mouse.GetState();
+            Point mousePosition = new Point(mouseState.X, mouseState.Y);
+            
+
+            //Hover menu item
+            if(Rectangles[0].Contains(mousePosition)){
+                labelIndex = 0;
+            }
+            else if(Rectangles[1].Contains(mousePosition)){
+                labelIndex = 1;
+            }
+
+            //Mouse Click
+            if (mouseState.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed){
+                if(Rectangles[0].Contains(mousePosition)){
+                    ScreenManager.ChangeScreen(new MainMenuScreen());
+                }
+                if(Rectangles[1].Contains(mousePosition)){
+                    Game1.Instance.Exit();
+                }
+            }
+
+            //Handle Keyboard Menu List Navigation
+            if(Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Up)){
+
+                labelIndex--;
+                if(labelIndex < 0) labelIndex = 0;
+
+            }
+            else if(Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Down)){
+
+                labelIndex++;
+                if(labelIndex > menuLabels.Length - 1) labelIndex = menuLabels.Length - 1;
+
+            }
             // press Enter to go to MainMenuScreen
             if (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Enter))
             {
-                ScreenManager.ChangeScreen(new MainMenuScreen());
+                if(labelIndex == 0){
+                    ScreenManager.ChangeScreen(new MainMenuScreen());
+                }
+                if(labelIndex == 1){
+                    Game1.Instance.Exit();
+                }
             }
-            if (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
-            {
-                Game1.Instance.Exit();
-            }
+
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            //Loop through rectangles to draw the menu
             for (int i = 0; i < Rectangles.Length; i++)
             {
-                spriteBatch.Draw(boxTexture, Rectangles[i], Color.Black);
+                //Pick the color based on hovered label
+                Color boxColor = (i == labelIndex) ? Color.Gray : Color.Black;
+                spriteBatch.Draw(boxTexture, Rectangles[i], boxColor);
                 Vector2 textSize = _font.MeasureString(menuLabels[i]);
                 spriteBatch.DrawString(_font, menuLabels[i], new Vector2(Rectangles[i].X + (Rectangles[i].Width - textSize.X) / 2, 
                 Rectangles[i].Y + (Rectangles[i].Height - textSize.Y) / 2), Color.White);
