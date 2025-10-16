9f7a60199653b2032b1fd78fa1d2fd3e6cf4332a
Harivansh Luchmun <hluchmun108@gmail.com>
2025-10-16 00:03:34 -0500

    Added player position to mainscreen

--- Screens/MainMenuScreen.cs
+++ Screens/MainMenuScreen.cs
@@ -7,26 +7,51 @@ namespace newgame.Screens;
     public class MainMenuScreen : Screen
     {
         private SpriteFont _font;
+        private Vector2 playerPosition;
 
         public override void LoadContent()
         {
             _font = Game1.Instance.Content.Load<SpriteFont>("File");
+            playerPosition = new Vector2(Game1.Instance.width / 2, Game1.Instance.height / 2);
         }
 
         public override void Update(GameTime gameTime)
         {
             // Example: press Escape to go back to StartScreen
 
-                        if (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
+            if (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
             {
-                ScreenManager.ChangeScreen(new TitleScreen());
+                //ScreenManager.ChangeScreen(new TitleScreen());
+            }
+
+
+            //Handle Player arrow key movement
+            if (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Left))
+            {
+                // Move player left
+                playerPosition.X -= 5f; // Move left by 5 units
+            }
+            else if (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Right))
+            {
+                // Move player right
+                playerPosition.X += 5f; // Move right by 5 units
+            }
+            else if (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Up))
+            {
+                // Move player up
+                playerPosition.Y -= 5f; // Move up by 5 units
+            }
+            else if (Microsoft.Xna.Framework.Input.Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Down))
+            {
+                // Move player down
+                playerPosition.Y += 5f; // Move down by 5 units
             }
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.DrawString(_font, "Welcome To Main Menu! Press Escape to return To TitleScreen. ", new Vector2(100, 100), Color.White);
-
+            spriteBatch.DrawString(_font, "Main Screen. ", new Vector2(100, 100), Color.White);
+            spriteBatch.DrawString(_font, "Player Position: " + playerPosition.ToString(), playerPosition, Color.White);
         }
     }
 
--- obj/Debug/net8.0/MyGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cbaa47dda4d5d1e74332317c37f269b77a3357e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fdcc720c77cf40f253560e346b55c0262887917")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
